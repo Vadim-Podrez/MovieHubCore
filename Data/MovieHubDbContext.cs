8f721e320a341bd9d0f5c7eee7ddd420e602a0d0
PrestoForIT <prestoforit@gmail.com>
2023-12-23 18:23:19 +0200

    fixing MovieHubDbContext.cs & Repertoire.cs)

--- Data/MovieHubDbContext.cs
+++ Data/MovieHubDbContext.cs
@@ -1,40 +1,46 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MovieHubCore.Models;
 
-namespace MovieHubCore.Domain;
-
-public class CinemaDbContext : DbContext
+namespace MovieHubCore.Domain
 {
-    public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
+    public class CinemaDbContext : DbContext
     {
-    }
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options) { }
 
-    public DbSet<Employee> Employees { get; set; }
-    public DbSet<Position> Positions { get; set; }
-    public DbSet<Genre> Genres { get; set; }
-    public DbSet<Movie> Movies { get; set; }
-    public DbSet<Repertoire> Repertoires { get; set; }
-    public DbSet<Seat> Seats { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Position> Positions { get; set; }
+        public DbSet<Genre> Genres { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Repertoire> Repertoires { get; set; }
+        public DbSet<Seat> Seats { get; set; }
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        // Здійсніть налаштування моделі, якщо потрібно
-        // Наприклад, встановлення зв'язків між таблицями
-        modelBuilder.Entity<Employee>()
-            .HasOne(e => e.Position)
-            .WithMany()
-            .HasForeignKey(e => e.PositionId);
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Встановлення первинних ключів
+            modelBuilder.Entity<Employee>().HasKey(e => e.EmployeeId);
+            modelBuilder.Entity<Position>().HasKey(p => p.PositionId);
+            modelBuilder.Entity<Genre>().HasKey(g => g.GenreId);
+            modelBuilder.Entity<Movie>().HasKey(m => m.MovieId);
+            modelBuilder.Entity<Repertoire>().HasKey(r => r.SessionId);
+            modelBuilder.Entity<Seat>().HasKey(s => s.SeatId);
 
-        modelBuilder.Entity<Movie>()
-            .HasOne(m => m.Genre)
-            .WithMany()
-            .HasForeignKey(m => m.GenreId);
+            // Встановлення зв'язків між таблицями
+            modelBuilder.Entity<Employee>()
+                .HasOne(e => e.Position)
+                .WithMany()
+                .HasForeignKey(e => e.PositionId);
 
-        modelBuilder.Entity<Seat>()
-            .HasOne(s => s.Employee)
-            .WithMany()
-            .HasForeignKey(s => s.EmployeeId);
+            modelBuilder.Entity<Movie>()
+                .HasOne(m => m.Genre)
+                .WithMany()
+                .HasForeignKey(m => m.GenreId);
 
-        base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Seat>()
+                .HasOne(s => s.Employee)
+                .WithMany()
+                .HasForeignKey(s => s.EmployeeId);
+
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
--- Models/Repertoire.cs
+++ Models/Repertoire.cs
@@ -1,8 +1,12 @@
-﻿namespace MovieHubCore.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieHubCore.Models
 {
     public class Repertoire
     {
         // Репертуар (Код сеансу, Дата, Час початку, Час, Ціна квитка) [10 записів]
+        [Key]
+        public int RepertoireId { get; set; }
         public int SessionId { get; set; }
         public DateTime Date { get; set; }
         public TimeSpan StartTime { get; set; }
